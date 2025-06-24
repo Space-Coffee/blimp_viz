c1e8ec405a8d5c5f9243da17b142d0f6485ad057
Tomasz Kawalec <tkawalec369@gmail.com>
2025-06-24 20:18:57 +0200

    Somehow make CesiumJS work

--- src/lib/CesiumMapView.svelte
+++ src/lib/CesiumMapView.svelte
@@ -1,21 +1,45 @@
 <script lang="ts">
 	import { onMount } from "svelte";
 	import * as Cesium from "cesium";
+	import "cesium/Build/Cesium/Widgets/widgets.css";
+
+	let cesiumContainer: HTMLElement;
 
 	let viewer: Cesium.Viewer;
-	onMount(async () => {
-		window.CESIUM_BASE_URL = "/";
+	onMount(() => {
+		window.CESIUM_BASE_URL = "./build";
+		/*Object.defineProperty(document, "CESIUM_BASE_URL", {
+			value: "build",
+		});*/
 
-		viewer = new Cesium.Viewer("cesium-container", {
-			terrain: Cesium.Terrain.fromWorldTerrain(),
-			timeline: false,
-			navigationHelpButton: false,
+		cesiumContainer.style.border = "5px solid red";
+		viewer = new Cesium.Viewer(cesiumContainer, {
+			// terrain: Cesium.Terrain.fromWorldTerrain(),
+			// terrainProvider: new Cesium.EllipsoidTerrainProvider({}),
+			// timeline: false,
+			// navigationHelpButton: false,
+			// geocoder: false,
+			/* baseLayer: new Cesium.ImageryLayer(
+				new Cesium.OpenStreetMapImageryProvider({
+					url: "https://tile.openstreetmap.org/",
+				}),
+			),*/
 		});
 	});
 </script>
 
-<div>
+<div style="width: 100%">
 	<h2>Cesium map view</h2>
 
-	<div id="cesium-container"></div>
+	<div id="cesiumContainer" bind:this={cesiumContainer}></div>
 </div>
+
+<style>
+	#cesiumContainer {
+		width: 100%;
+		height: 100%;
+		margin: 0;
+		padding: 0;
+		overflow: hidden;
+	}
+</style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,32 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import viteStaticCopy from 'vite-plugin-static-copy'
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+import path from 'path';
 
 const cesiumBuildPath = 'node_modules/cesium/Build/Cesium/'
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [
+		sveltekit(),
+		viteStaticCopy({
+			targets: [
+				{
+					src: path.join(cesiumBuildPath, 'Assets'),
+					dest: 'build'
+				},
+				{
+					src: path.join(cesiumBuildPath, 'ThirdParty'),
+					dest: 'build'
+				},
+				{
+					src: path.join(cesiumBuildPath, 'Widgets'),
+					dest: 'build'
+				},
+				{
+					src: path.join(cesiumBuildPath, 'Workers'),
+					dest: 'build'
+				},
+			]
+		})
+	]
 });
